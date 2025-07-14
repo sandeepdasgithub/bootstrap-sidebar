dc3b228401a839a7a21420ff42acd1e0aa23a142
sandeepdasgithub <145173844+sandeepdasgithub@users.noreply.github.com>
2025-07-14 12:45:59 +0530

    Create sidebar.blade.php
    sidebar

---
+++ sidebar.blade.php
@@ -0,0 +1,8 @@
+<div id="sidebar-wrapper">
+    <div class="sidebar-heading p-3 border-bottom bg-light">MyApp</div>
+    <div class="list-group list-group-flush">
+        <a href="#" class="list-group-item list-group-item-action list-group-item-light p-3">Dashboard</a>
+        <a href="#" class="list-group-item list-group-item-action list-group-item-light p-3">Projects</a>
+        <a href="#" class="list-group-item list-group-item-action list-group-item-light p-3">Tasks</a>
+    </div>
+</div>
